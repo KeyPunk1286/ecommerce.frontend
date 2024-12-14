d93d785b57a464570efd4cc92623590cb8704957
keyPunk <kravtsov1286roman@gmail.com>
2024-12-14 15:01:08 +0200

    EF#15

--- composables/useLocalStorage.js
+++ composables/useLocalStorage.js
@@ -1,27 +1,36 @@
 export const useLocalStorage = () => {
-    const setItem = (key, value) => {
-        try {
-            const serializedValue = JSON.stringify(value);
-            localStorage.setItem(key, serializedValue);
-        } catch (error) {
-            console.error("Error saving in localStorage", error);
-        }
-    };
+  const setItem = (key, value) => {
+    try {
+      const serializedValue = JSON.stringify(value);
+      localStorage.setItem(key, serializedValue);
+    } catch (error) {
+      console.error("Error saving in localStorage", error);
+    }
+  };
 
-    const getItem = (key) => {
-        try {
-            const storedValue = localStorage.getItem(key);
-            return storedValue ? JSON.parse(storedValue) : null;
-        } catch (error) {
-            console.error("Error getting from localStorage", error);
-            return null;
-        }
-    };
+  const getItem = (key) => {
+    try {
+      const storedValue = localStorage.getItem(key);
+      return storedValue ? JSON.parse(storedValue) : null;
+    } catch (error) {
+      console.error("Error getting from localStorage", error);
+      return null;
+    }
+  };
 
-    return {
-        setItem,
-        getItem
-    };
+  const removeItem = (key) => {
+    try {
+      localStorage.removeItem(key);
+    } catch (error) {
+      console.error("Error removing from localStorage", error);
+    }
+  };
+
+  return {
+    setItem,
+    getItem,
+    removeItem,
+  };
 };
 
 export default useLocalStorage;
